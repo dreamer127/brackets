12617bf2dd68add2279290286509d7dd7a4f02a4
Rustam Faradjev <rystam9614@gmail.com>
2019-03-19 23:50:55 +0300

    ficha

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLength, regex, pairsBrackets, numb;
+  if (str.length % 2 != 0){
+    return false;
+  }
+  do {
+    strLength = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      numb = +bracketsConfig[i].join('');
+      if (numb == bracketsConfig[i].join(''))
+        pairsBrackets = bracketsConfig[i].join('');
+      else
+        pairsBrackets = "\\" + bracketsConfig[i][0] + "\\" + bracketsConfig[i][1];
+      regex = new RegExp(pairsBrackets, 'g');
+      str = str.replace(regex, '');
+    }
+    } 
+  while (strLength != str.length);
+
+  if(str.length === 0) {
+      return true;
+    }
+    else
+      return false;
 }
